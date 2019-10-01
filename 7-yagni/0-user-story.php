186660feac0142a0ea7191cfe2fbd722a443c2ec
PG <masterpiece.prg@gmail.com>
2019-10-01 22:51:35 +0530

    YAGNI - sample user story with incorrect and correct examples

---
+++ 7-yagni/0-user-story.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| YAGNI - You Ain't Gonna Need It
+|--------------------------------------------------------------------------
+|
+| * is trying to save us time and focus on most important things in our todo.
+|
+| * if its not needed now, it may never be needed later.
+|
+| * the requirements could potentially change in the future.
+|
+| * always implement things when you actually need them, never when you just foresee them
+*/
+
+/*
+|--------------------------------------------------------------------------
+| As a user, I should see the list of resources
+|--------------------------------------------------------------------------
+|
+| The resources are saved in a database and through a handler, I should be
+| be able to get the list of resources objects, so then I could list them on
+| the user interface.
+|
+| It is important to note that these resources would potentially be between
+| 10 - 50 items maximum.
+|
+|
+| Acceptance Criteria
+| -------------------
+| * Implement a resource handler to get the list of resources
+| * The list of resources can be objects to be later displayed in a user interface
+|
+*/
